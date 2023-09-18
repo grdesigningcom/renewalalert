870b200dbca4a493a4f545624ff39527719c0af1
me-miheer <me.miheer@gmail.com>
2023-09-18 20:03:14 +0530

    title changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Dev - Renewal alets</title>
+        <title>Prod - Renewal alets</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
